2431ddabcc003beca433d7eadee7d1e4dcb73d76
Codec-V <vedyamgantiwar@gmail.com>
2025-03-17 20:45:19 +0530

    Projects repo added
    This repo contains some of mine web development projects

---
+++ Spotify clone/spotify.js
@@ -0,0 +1,4 @@
+// Optional: Show an alert when the "Create playlist" button is clicked
+document.querySelector('.playlist button').addEventListener('click', () => {
+    alert('Playlist created!');
+});
